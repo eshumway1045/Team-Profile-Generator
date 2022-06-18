89556cb88a7127f7c3adaebf12c05d22d4097d82
Eric Shumway <ericshumway2@mail.weber.edu>
2022-06-18 10:25:53 -0600

    cleaning up unused code

--- index.js
+++ index.js
@@ -71,60 +71,6 @@ function appMenu() {
             }
         }
     ])
-        // inquirer.prompt([
-        //     {
-        //         type: "input",
-        //         name: "managerName",
-        //         message: "What is the team manager's name?",
-        //         validate: answer => {
-        //             if (answer !== "") {
-        //                 return true;
-        //             }
-        //             return "Please enter at leaset one Character.";
-        //         }
-        //     },
-        //     {
-                // type: "input",
-                // name: "managerId",
-                // message: "What is the team maanager's id?",
-                // validate: answer => {
-                //     const pass = answer.match(
-                //         /^[1-9]\d*$/
-                //     );
-                //     if (pass) {
-                //         return true;
-                //     }
-                //     return "Please enter a positive number greater than zero.";
-                // }
-        //     },
-        //     {
-        //         type: "input",
-        //         name: "managerEmail",
-        //         message: "What is the team manager's email?",
-        //         validate: answer => {
-        //             const pass = answer.match(
-        //                 /\S+@\S+\.\S+/
-        //             );
-        //             if (pass) {
-        //                 return true;
-        //             }
-        //             return "Please enter a valid email address.";
-        //         }
-        //     },
-        //     {
-        //         type: "input",
-        //         name: "managerOfficeNumber",
-        //         message: "What is the team manager's office number?",
-        //         validate: answer => {
-        //             const pass = answer.match(
-        //                 /^[1-9]\d*$/
-        //             );
-        //             if (pass) {
-        //                 return true;
-        //             }
-        //             return "Please enter a positive number greater than zero.";
-        //         }
-        //     }
         .then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             teamMembers.push(manager);
